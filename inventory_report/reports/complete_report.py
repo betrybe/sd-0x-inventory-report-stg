761933a7aa01b6c735065fefd4f3bd35badc80f0
“brunobatista” <brunobatista101@gmail.com>
2020-12-07 09:39:12 -0300

    passando tudo

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,15 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products):
+        simple = super().generate(products)
+        data = Counter(prod["nome_da_empresa"] for prod in products)
+        storage_prod = "Produtos Estocados por empresa: \n"
+
+        for key, value in data.items():
+            storage_prod += f"- {key}: {value}\n"
+
+        return f"{simple}\n{storage_prod}"
